81bf7e6c041fce4c5249930e9445feda53f2fab2
Suraj Nath <devversitymail@gmail.com>
2018-10-27 03:13:56 +0530

    Style and wire up search

--- youtube_video_player/src/index.js
+++ youtube_video_player/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -14,8 +15,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch('Prison Mike')
+  }
 
-    YTSearch({key: API_KEY, term: 'The Prison Mike'}, (videos) => {
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -24,9 +28,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
