58be82341cfcf073147181306879e3f89e00e241
Suraj Nath <devversitymail@gmail.com>
2018-10-31 02:12:48 +0530

    Revoke, Ignore and Remove YouTube API Key

--- youtube_video_player/src/index.js
+++ youtube_video_player/src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyD4U_R2xThZqh3pjTw4v3Sy-bbgbo-BLQE';
+import {API_KEY} from "../keys"
 
 class App extends Component {
   constructor(props) {
